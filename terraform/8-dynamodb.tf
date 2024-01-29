8d5e1b32bcc98d4b3003306abaf6d3afae9e9cfd
Rodrigo Carmo <rodrigocarmodev@gmail.com>
2024-01-29 17:50:31 -0300

    Aumentado capacidade de leitura e de gravação por segundo do dynamodb

--- terraform/8-dynamodb.tf
+++ terraform/8-dynamodb.tf
@@ -2,8 +2,8 @@
 resource "aws_dynamodb_table" "pagamentos" {
   name           = "pagamentos"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
+  read_capacity  = 50
+  write_capacity = 50
   hash_key       = "IdTransacao"
 
   attribute {
